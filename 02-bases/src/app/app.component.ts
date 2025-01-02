c6f66f416b249f400574fa7837b3ebc8d555346c
estiven2011 <juan_posada23221@elpoli.edu.co>
2025-01-02 18:02:37 -0500

    contador, desde el app cpmponent

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'bases';
+  title:string = 'bases';
+
+  counter:number = 0
+
+  funcionalidad(value: number): void {
+    if(this.counter + value >= 0 ){
+      this.counter += value
+    }
+  }
+
+  reset(){
+    this.counter = 0
+    console.log(this.counter);
+    
+  }
 }
